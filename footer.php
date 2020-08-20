01494c1ae9d64ea008996e3e53f07dad929f395f
Jonathan <tangjon@hotmail.com>
2020-08-20 13:16:46 -0700

    support gallery from customizer

--- footer.php
+++ footer.php
@@ -40,10 +40,6 @@
                         </li
                     </ul>
                 </div>
-                <div class="col-sm-4">
-                    <h3>About Freelancer</h3>
-                    <p>Freelance is a free to use, open source Bootstrap theme created by <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
-                </div>
             </div>
         </div>
     </div>
