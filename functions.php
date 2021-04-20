5fc13c08470920686026f57552453b604fb9bfad
Jonathan Tang <jct19@sfu.ca>
2021-04-20 13:26:08 -0700

    fix pages

--- functions.php
+++ functions.php
@@ -93,18 +93,16 @@ add_action( 'after_setup_theme', 'tangyjon_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function tangyjon_widgets_init() {
+function scout33_sidebar_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'tangyjon' ),
 		'id'            => 'sidebar-1',
 		'description'   => esc_html__( 'Add widgets here.', 'tangyjon' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
 }
-add_action( 'widgets_init', 'tangyjon_widgets_init' );
+add_action( 'widgets_init', 'scout33_sidebar_init' );
 
 /**
  * Enqueue scripts and styles.
